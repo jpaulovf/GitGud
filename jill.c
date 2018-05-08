bc62749d78920bcef78c5d39bf5059f108474b15
Joao <jpaulovf@gmail.com>
2018-05-08 14:47:50 -0300

    Problema do flavor resolvido

--- jill.c
+++ jill.c
@@ -3,13 +3,13 @@
 int main(int argc, char *argv[]){
    
    // Checking number of args
-   if (argc != 2){
-      printf("Error: number of ags must be 1!\n");
-      printf("Usage: %s <your_name>\n", argv[0]);
+   if (argc != 3){
+      printf("Error: number of ags must be 2!\n");
+      printf("Usage: %s <your_name> <your_flavor>\n", argv[0]);
       return -1;
    }
 
-   printf("%s Sandwich!\n", argv[1]);
+   printf("%s %s Sandwich!\n", argv[1], argv[2]);
 
    return 0;
 
