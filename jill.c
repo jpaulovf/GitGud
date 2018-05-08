226874d9a9394b1ca10304ca2f44f27444c91aca
Joao <jpaulovf@gmail.com>
2018-05-08 13:14:27 -0300

    Adicionados os parâmetros de linha de comando

--- jill.c
+++ jill.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[]){
    
-   printf("Jill Sandwich!\n");
+   // Checking number of args
+   if (argc != 2){
+      printf("Error: number of ags must be 1!\n");
+      printf("Usage: %s <your_name>\n", argv[0]);
+      return -1;
+   }
+
+   printf("%s Sandwich!\n", argv[1]);
 
    return 0;
 
